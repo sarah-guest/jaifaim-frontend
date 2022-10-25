e996b9b510ef29b8db840475d17dc5c385336acc
Tramio <tramiodev@outlook.fr>
2022-10-25 17:00:48 +0200

    add tab navigation

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import user from './reducers/user';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// STYLE NAVIGATION BAR
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 // SCREENS
 import LandingScreen from './screens/LandingScreen';
 import DemoScreen from './screens/DemoScreen';
@@ -13,22 +15,97 @@ import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import EaterProviderScreen from './screens/EaterProviderScreen';
 import MapScreen from './screens/MapScreen';
-
-const Stack = createNativeStackNavigator();
+// // SCREENS POUR TAB
+import ProfileScreen from './screens/ProfileScreen';
+import SnapScreen from './screens/SnapScreen';
+// REDUX
 const store = configureStore({
   reducer: { user },
 });
+const getIconName = (routeName) => {
+  switch (routeName) {
+    case 'Home': // commun: feed pour users, dashboard pour restaurants
+      return 'home';
+    case 'Map': // users: vue restaurants
+      return 'map';
+    case 'Profile': // commun
+      return 'user';
+    case 'Snap': // restaurants: prise photo plat du jour
+      return 'camera';
+    case 'Questions': // restaurants: Q&A
+      return 'comments';
+  }
+};
+// NAVIGATION
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Ce composant renvoie la TabNavigation pour restaurant OU user selon le reducer
+const TabNavigation = () => {
+  // RESTAURANT NAVIGATION
+  const RestaurantNavigation = (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome
+            name={getIconName(route.name)}
+            size={size}
+            color={color}
+          />
+        ),
+        // INACTIVE TABS COLORS
+        tabBarInactiveBackgroundColor: '#7F5539',
+        tabBarInactiveTintColor: '#DDB892',
+        // ACTIVE TABS COLORS
+        tabBarActiveBackgroundColor: '#DDB892',
+        tabBarActiveTintColor: '#7F5539',
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={LandingScreen} />
+      <Tab.Screen name="Snap" component={SnapScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+
+  // USERNAVIGATION
+  const UserNavigation = (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome
+            name={getIconName(route.name)}
+            size={size}
+            color={color}
+          />
+        ),
+        tabBarInactiveBackgroundColor: '#7F5539',
+        tabBarInactiveTintColor: '#DDB892',
+        tabBarActiveBackgroundColor: '#DDB892',
+        tabBarActiveTintColor: '#7F5539',
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={LandingScreen} />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+
+  return UserNavigation; // Todo: mettre condition ici pour renvoyer navigation user ou resto !
+};
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
-          <Stack.Screen name="Map" component={MapScreen} />
+          <Stack.Screen name="TabNavigation" component={TabNavigation} />
           <Stack.Screen name="Landing" component={LandingScreen} />
           <Stack.Screen name="Demo" component={DemoScreen} />
           <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="Map" component={MapScreen} />
           <Stack.Screen name="EaterProvider" component={EaterProviderScreen} />
         </Stack.Navigator>
       </NavigationContainer>
