7f195287b3e5e5b75488433597e99902e3f3ece2
Chloe <chloeheaulme@gmail.com>
2022-10-25 15:04:00 +0200

    branche faq

---
+++ screens/FaqScreen.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+export default function FaqScreen({ navigation }) {
+    return (
+      <View style={styles.container}>
+        
+          <TextInput style={styles.input}> 🔎 Recherche </TextInput>
+          <Text style={styles.text}> FAQ </Text>
+          
+        
+        
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'red',
+    },
+    text: {
+        backgroundColor: 'blue',
+        
+        
+        
+    },
+    input: {
+        height: 50,
+        width: '90%',
+        backgroundColor: '#E6CCB2',
+        alignItems : 'center',
+        textAlign: 'center',
+        borderRadius: 50,
+        
+        // marginBottom: 650,
+
+
+    }
+  });
