9f607405eb2843ce6ab302401fdd4c2137928cc7
Chloe <chloeheaulme@gmail.com>
2022-10-26 17:51:55 +0200

    profile screen fetch en cours

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,50 @@
 // IMPORTS HABITUELS
 import { Image, StyleSheet, Text, View } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
  
 // DUMMY RESOURCES
 const restaurantAvatarSource = require('../assets/images/avatarRestaurant.png');
-const name = 'user';
-const bioShort =
-  '';
-const adresse = '';
-const website = 'https://www.lacapsule.academy/'; // Todo : http/https links in react native
-// ---------------
+const name = '';
+const path = 'restaurants';
 
-export default function ProfileScreen() {
+
+
+export default function ProfileScreen({ navigation, route }) {
+
+ const dispatch = useDispatch();
+ let { type } = route.params;
+ const [user, setUser] = useState(''); //nom de l'utilisateur
+ const [profile, setProfile] = useState(''); //Profil
+
+ fetch(`http://${IP_ADDRESS}:3000/${path}/restaurant`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        user: setUser,
+        profile: setProfile,
+      }),
+    }).then(response => response.json())
+      .then(data => {
+
+       
+        if (data.result) {
+          if (type === 'user') {
+            dispatch(({ user: setUser, token: data.token }));
+            navigation.navigate( { setUser });
+          } else if (type === 'restaurant') {
+            dispatch(({ profile: setProfile, token: data.token }));
+            navigation.navigate('Welcome', { type: 'restaurant' });
+          }
+         
+        
+        else {}
+      };
+  })};
+
+ 
+
   const RestaurantProfileScreen = () => {
     return (
       <View style={styles.container}>
@@ -26,8 +58,8 @@ export default function ProfileScreen() {
     );
   };
 
-  return <RestaurantProfileScreen />;
-}
+  return <RestaurantProfileScreen/>;
+
 
 const styles = StyleSheet.create({
   container: {
@@ -43,7 +75,7 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 32,
     marginTop: 42,
-    flexdirection: 'column',
+    
     
   },
   bioShort: {
