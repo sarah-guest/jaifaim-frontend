f2f0b7f08854c9f37f2cfbf8e1702fd13caecc1c
Tramio <tramiodev@outlook.fr>
2022-10-26 18:34:32 +0200

    create "tag" component

---
+++ components/Tag.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import convertColor from '../modules/convertColor';
+
+export default function OurButton(props) {
+  const { isPressed, onPress, text } = props;
+
+  const isPressedStyle = isPressed && {
+    backgroundColor: convertColor('cannelle'),
+    color: convertColor('sable'),
+  };
+
+  return (
+    <TouchableOpacity>
+      <Text style={[styles.tag, isPressedStyle]} onPress={onPress}>
+        {text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  tag: {
+    marginRight: 6,
+    paddingVertical: 6,
+    paddingHorizontal: 16,
+    borderRadius: 100,
+    backgroundColor: convertColor('caféaulaitfroid'),
+    color: convertColor('marronfoncé'),
+  },
+});
