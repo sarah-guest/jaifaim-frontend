9f7715f3d3cb912df452f3ec7456e40689b2085f
Tramio <tramiodev@outlook.fr>
2022-10-26 11:26:59 +0200

    add camera component to snapscreen

--- screens/SnapScreen.js
+++ screens/SnapScreen.js
@@ -1,11 +1,43 @@
 // IMPORTS HABITUELS
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+// IMPORTS APPAREIL PHOTO
+import { Camera } from 'expo-camera';
+import { useIsFocused } from '@react-navigation/native';
+import OurButton from '../components/Button';
 
 export default function SnapScreen() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const isFocused = useIsFocused();
+  let cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    const photo = await cameraRef.takePictureAsync({ quality: 0.3 });
+    console.log(photo);
+  };
+
+  if (!hasPermission || !isFocused) {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Turn on camera permission to make the most of our app's awesomeness.
+          🤎
+        </Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Tab 2!</Text>
-    </View>
+    <Camera style={styles.camera} ref={(ref) => (cameraRef = ref)}>
+      <OurButton icon="camera" color="cannelle" onPress={takePicture} />
+    </Camera>
   );
 }
 
@@ -15,5 +47,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'lavender',
+  },
+  camera: {
+    padding: 16,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
 });
