d3178c801483b679c32494dc2f55e7bd0957a3b8
Chloe <chloeheaulme@gmail.com>
2022-10-26 14:43:45 +0200

    profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,8 @@
 // IMPORTS HABITUELS
 import { Image, StyleSheet, Text, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+ 
 // DUMMY RESOURCES
 const restaurantAvatarSource = require('../assets/images/avatarRestaurant.png');
 const name = 'user';
@@ -14,10 +16,12 @@ export default function ProfileScreen() {
   const RestaurantProfileScreen = () => {
     return (
       <View style={styles.container}>
+        <View style={styles.view}> 
         <Image source={restaurantAvatarSource} />
         <Text style={styles.name}>{name}</Text>
         <Text style={styles.underName}>{adresse}</Text>
         <Text style={styles.bioShort}>{bioShort}</Text>
+        </View>
       </View>
     );
   };
@@ -27,17 +31,31 @@ export default function ProfileScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 32,
+    
+    padding: 110,
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'left',
     alignItems: 'center',
     backgroundColor: '#E6CCB3',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   name: {
     fontSize: 32,
+    marginTop: 42,
+    flexdirection: 'column',
+    
   },
   bioShort: {
     fontSize: 18,
     textAlign: 'center',
   },
+  view: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 122,
+    borderradius: 50,
+    
+  
+  }
 });
