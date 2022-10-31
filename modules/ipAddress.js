ab65b4c6f77ef2c6f5e3edefbfea5a74d47c80c9
Chloe <chloeheaulme@gmail.com>
2022-10-31 14:46:29 +0100

    profile screen

--- modules/ipAddress.js
+++ modules/ipAddress.js
@@ -1,9 +1,9 @@
 const clara = '182';
 const lydie = '166';
 const sarah = '158';
-const chloé = '122';
+const chloe = '122';
 
-const user = clara;
+const user = chloe;
 const IP_ADDRESS = `192.168.10.${user}`;
 
 export default IP_ADDRESS;
