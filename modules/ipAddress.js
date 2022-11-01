e46822918d2a935f0bb201e284adf937fc7ab522
Lydie Paulo <lydiepaulo.ui@gmail.com>
2022-11-01 10:57:54 +0100

    add swiper

--- modules/ipAddress.js
+++ modules/ipAddress.js
@@ -4,6 +4,7 @@ const sarah = '158';
 const chloe = '122';
 
 const user = sarah;
-const IP_ADDRESS = `192.168.10.${user}`;
+//const IP_ADDRESS = `192.168.10.${user}`;
+const IP_ADDRESS = `192.168.1.36`;
 
 export default IP_ADDRESS;
--- screens/DemoScreen.js
+++ screens/DemoScreen.js
@@ -1,18 +1,70 @@
-import { Button, Text, View, StyleSheet } from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+} from 'react-native';
+//import swiper
+import Swiper from 'react-native-swiper';
 import OurButton from '../components/Button';
+//nos imports
+import Title from '../components/Title';
+
 export default function DemoScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>DemoScreen</Text>
-      <OurButton text="Sign Up" onPress={() => navigation.navigate('EaterProvider')} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Title h1 isCentered={true}>Tutoriel</Title>
+      </View>
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Image source={require('../assets/images/DEMO_screen1.png')} style={styles.image} />
+        </View>
+        <View style={styles.slide2}>
+          <Image source={require('../assets/images/DEMO_screen2.png')} style={styles.image} />
+        </View>
+        <View style={styles.slide3}>
+          <Image source={require('../assets/images/DEMO_screen3.png')} style={styles.image} />
+        </View>
+      </Swiper>
+
+      <OurButton color='caféaulaitchaud' text="Ok, j'arrive !" onPress={() => navigation.navigate('EaterProvider')} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',  
+    justifyContent: 'center',
     alignItems: 'center',
   },
+  title: {
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  //swiper styles: 
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  slide2: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  slide3: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  image: {
+    width: 250,
+    height: 550,
+    borderRadius: 20,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
 });
