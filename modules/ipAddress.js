22029e36feaf1d0cf36f514af6a1ec55c5252577
Chloe <chloeheaulme@gmail.com>
2022-11-02 09:36:48 +0100

    ok

--- modules/ipAddress.js
+++ modules/ipAddress.js
@@ -3,7 +3,7 @@ const lydie = '166';
 const sarah = '158';
 const chloe = '122';
 
-const user = sarah;
-//const IP_ADDRESS = `192.168.10.${user}`;
-const IP_ADDRESS ='192.168.0.20'
+const user = chloe;
+const IP_ADDRESS = `192.168.10.${user}`;
+//const IP_ADDRESS ='192.168.0.20'
 export default IP_ADDRESS;
