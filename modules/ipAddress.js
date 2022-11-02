650509f1ffb8fb355663135015af5bf9823245e6
sarah-guest <sarah.guest9@gmail.com>
2022-11-02 09:19:15 +0100

    test

--- modules/ipAddress.js
+++ modules/ipAddress.js
@@ -4,6 +4,6 @@ const sarah = '158';
 const chloe = '122';
 
 const user = sarah;
-//const IP_ADDRESS = `192.168.10.${user}`;
-const IP_ADDRESS ='192.168.0.20'
+const IP_ADDRESS = `192.168.10.${user}`;
+//const IP_ADDRESS ='192.168.0.20'
 export default IP_ADDRESS;
