0663d25b8842ffb10b7a9c6c676771bbc5e21643
sarah-guest <sarah.guest9@gmail.com>
2022-11-03 12:36:24 +0100

    faq

--- screens/FaqScreen.js
+++ screens/FaqScreen.js
@@ -1,5 +1,6 @@
 import {
   StyleSheet,
+  Modal,
   Text,
   TextInput,
   TouchableOpacity,
@@ -18,63 +19,87 @@ import IP_ADDRESS from '../modules/ipAddress';
 //Import de nos composants
 import OurTextInput from '../components/TextInput';
 import OurButton from '../components/Button';
-
+import convertColor from '../modules/convertColor';
+import OurText from '../components/OurText';
+import Title from '../components/Title';
+//Import FontAwesome
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 export default function FaqScreen({ navigation, route }) {
-  const restaurant = useSelector((state) => state.restaurant.value);
-  const [answer, setAnswer] = useState('');
-  const [question, setQuestion] = useState('');
-  const [date, setDate] = useState('2050-11-22T23:59:59');
 
- // let { type } = route.params;
-    
-	useEffect(() => {
-		setDate(new Date());
-	}, []);
-  const askQuestion = () => {
-    
-      fetch(`http://${IP_ADDRESS}:3000/askquestion/:token`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-           date: Date,
-           message: question,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.message, "C'est la réponse!")
-          if (type === 'user') {
-           
-            if(data.result){
-            setQuestion('');
-          }}
-        });
-    
+
+  const [isHidden, setIsHidden] = useState(false);
+  //fonction qui fait apparaitre ou disparaitre la réponse
+  const handleToggle = () => {
+    setIsHidden(!isHidden);
   };
+
 
   return (
     <ImageBackground
       source={require('../assets/images/background.jpg')}
       style={styles.background}
       blurRadius={60}
+
     >
+
+
+
+
       <SafeAreaView style={styles.container}>
         <SearchBar />
-        <OurTextInput
-          placeholder="Postez votre réponse"
-          onChangeText={(value) => setQuestion(value)}
-          value={question}
-        />
-        <OurButton
-          onPress={askQuestion}
-          text="Pose ta question"
-          color="caféaulaitchaud"/>
-        < View style={styles.question}>
+        <Title h1 isLight={true}>FAQ </Title>
 
+
+
+
+        <View style={styles.questionAnswer}>
+          <View style={styles.question}>
+            <FontAwesomeIcon style={styles.plus}
+              name='plus'
+              size={30}
+              onPress={() => handleToggle()} />
+            <OurText body1>Votre restaurant est-il ouvert le dimanche?
+            </OurText>
+          </View>
+          {isHidden === true &&
+            <View style={styles.answer}><OurText body1>Reponse: </OurText>
+              <OurText body2>Nous sommes ouverts du mercredi au dimanche de 9h à minuit.</OurText>
+            </View>
+          }
         </View>
+        <View style={styles.questionAnswer}>
+          <View style={styles.question}>
+            <FontAwesomeIcon style={styles.plus}
+              name='plus'
+              size={30}
+              onPress={() => handleToggle()} />
+            <OurText body1>Est-ce que vous avez une formule déjeuner?
+            </OurText>
+          </View>
+          {isHidden === true &&
+            <View style={styles.answer}><OurText body1>Reponse: </OurText>
+              <OurText body2>Oui, 11,50€ le plat, 13,50€ entrée/plat ou plat/dessert et 16€ pour la formule entière.</OurText>
+            </View>
+          }
+        </View>
+        <View style={styles.questionAnswer}>
+          <View style={styles.question}>
+            <FontAwesomeIcon style={styles.plus}
+              name='plus'
+              size={30}
+              onPress={() => handleToggle()} />
+            <OurText body1>Votre restaurant est-il ouvert le dimanche?
+            </OurText>
+          </View>
+          {isHidden === true &&
+            <View style={styles.answer}><OurText body1>Reponse: </OurText>
+              <OurText body2>Nous sommes ouverts du mercredi au dimanche de 9h à minuit.</OurText>
+            </View>
+          }
+        </View>
+
+
 
 
 
@@ -92,20 +117,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   container: {
-
     justifyContent: 'center',
     alignItems: 'center',
 
   },
-  text: {
-    backgroundColor: 'blue',
+  questionAnswer: {
+    justifyContent: 'center',
+    backgroundColor: convertColor('sable'),
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginTop: 20,
+    width: '80%'
+
   },
-  input: {
-    height: 50,
-    width: '90%',
-    backgroundColor: '#E6CCB2',
+  question: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    textAlign: 'center',
-    borderRadius: 50,
+    margin: 10,
+
+  },
+  plus: {
+    paddingRight: 15,
+    justifyContent: 'center',
+  },
+  answer: {
+    paddingLeft: 36,
+    paddingTop: 10,
+    paddingBottom: 15,
+    backgroundColor: convertColor('caféaulaitchaud'),
+  },
+  modalView: {
+    flex: 1,
+    borderRadius: 10,
+    backgroundColor: convertColor('caféaulaitchaud'),
+    flexDirection: 'row',
+    margin: 10,
+    justifyContent: 'center',
+
   },
 });
